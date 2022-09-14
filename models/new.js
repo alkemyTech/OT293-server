177d98e76a25f7a7e0a43e49aa3aa6c3feaabb4a
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-09-14 13:04:58 -0500

    model, controller and migration for news

---
+++ models/new.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+
+  class New extends Model {
+
+    static associate(models) {
+      // Define association here
+    }
+
+  }
+
+  New.init({
+    name: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    image: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'New'
+  });
+
+  return New;
+
+}
