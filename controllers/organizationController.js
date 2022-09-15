129872f65629dc55f64cff872469e1ee6fa380d0
lucasambrosini1 <87482540+lucasambrosini1@users.noreply.github.com>
2022-09-15 00:22:58 -0300

    Add organization template controller

---
+++ controllers/organizationController.js
@@ -0,0 +1,19 @@
+const { Organization } = require('../models/organization');
+
+class OrganizationController {
+  static async findAll(req, res, next) {
+
+  }
+
+  static async findOne(req, res, next) {
+
+  }
+
+  static async update(req, res, next) {
+
+  }
+
+  static async delete(req, res, next) {
+
+  }
+}
