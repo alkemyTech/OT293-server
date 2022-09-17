613d2c1de24bbc0608581373d34f943f98f9f680
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-09-17 01:52:35 -0300

    Add model and soft delete

--- models/user.js
+++ models/user.js
@@ -1,14 +1,8 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       User.belongsTo(models.Role, {as: 'role'});
     }
@@ -24,6 +18,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    paranoid: true
   });
+  
   return User;
 };
