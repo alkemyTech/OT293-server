20384ed2837d0c45a684659ad9a70388837fab38
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-09-17 03:15:26 -0300

    Add migration activities

---
+++ migrations/20220917061119-create-activities.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('activities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.CHAR
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.CHAR
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('activities');
+  }
+};
