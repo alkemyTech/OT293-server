7ce3e4ce083e995fc1b1015758a4dac1bd42fc68
camila <camilafkapp@gmail.com>
2022-09-20 11:06:15 -0300

    updating

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2,7 +2,7 @@ const {User} = require('../models/user');
 
 const verifyAdmin = async (req, res, next) => {
     const user = await User.findById(req.roleId)
-    if (user === 1) {
+    if (user === 'Admin') {
        next()
     } else {
         return res.status(403).json({message: 'You are not authorized to access this resource'})
