ee574a8c0035eebfc3d695d11a21bc786871d97c
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-09-21 14:52:30 -0500

    endpoint to delete any user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const db = require('../models/index');
+
 class UserController {
     constructor() { }
 
@@ -25,7 +27,18 @@ class UserController {
 
     // Delete User from DB
     // Method: DELETE
-    static async deleteUser(req, res) { }
+    static async deleteUser(req, res) {
+
+        const { id } = req.params;
+
+        const deletedUser = await db.User.destroy({
+            where: {
+                id
+            }
+        });
+
+        res.json({data: {id: deletedUser}});
+    }
 }
 
 module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+'use strict';
+
 var express = require('express');
+
+const UserController = require('../controllers/user.controller');
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
