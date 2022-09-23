5a132da1810d2cbb9d587e97d9ef6b7c1f13119f
lucasambrosini1 <87482540+lucasambrosini1@users.noreply.github.com>
2022-09-23 01:52:59 -0300

    Fix delete route

--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const CategoriesController = require('../controllers/organizationController');
 const verifyAdmin = require('../middlewares/admin');
 
-router.delete('/delete/:id', verifyAdmin, CategoriesController.delete);
+router.delete('/:id/', verifyAdmin, CategoriesController.delete);
 
 module.exports = router;
