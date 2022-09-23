a16ab966f09781a183ba0a2997437908d3e78cbb
lucasambrosini1 <87482540+lucasambrosini1@users.noreply.github.com>
2022-09-23 01:47:29 -0300

    Add jwt in register user response

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,48 +1,53 @@
-"use strict";
-
 const db = require('../models/index');
+const Jwt = require('../utils/jwt');
 
 class UserController {
-    constructor() { }
+  constructor() { }
 
-    // Get all user
-    // Method: GET
-    static async getUsers(req, res) { }
+  // Get all user
+  // Method: GET
+  static async getUsers(req, res) { }
 
-    // Get User by id
-    // Method: GET
-    static async getUserById(req, res) { }
+  // Get User by id
+  // Method: GET
+  static async getUserById(req, res) { }
 
-    // Create new User
-    // Method: POST
-    static async createUser(req, res) { }
-
-    // Update all User data
-    // Method: PUT
-    static async updateUser(req, res) { }
-
-    // Partially update User data
-    // Method: PATCH
-    static async partialUpdateUser(req, res) { }
-
-    // Delete User from DB
-    // Method: DELETE
-    static async deleteUser(req, res, next) {
-
-        try {
-            const { id } = req.params;
-
-            const deletedUser = await db.User.destroy({
-                where: {
-                    id
-                }
-            });
-
-            res.json({ data: { id: deletedUser } });
-        } catch (error) {
-            next(error);
-        }
+  // Create new User
+  // Method: POST
+  static async createUser(req, res, next) {
+    try {
+      const token = await Jwt.tokenSign({ ...req.body });
+      res.json({ data: { token } });
+    } catch (e) {
+      next(e);
     }
+  }
+
+  // Update all User data
+  // Method: PUT
+  static async updateUser(req, res) { }
+
+  // Partially update User data
+  // Method: PATCH
+  static async partialUpdateUser(req, res) { }
+
+  // Delete User from DB
+  // Method: DELETE
+  static async deleteUser(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const deletedUser = await db.User.destroy({
+        where: {
+          id,
+        },
+      });
+
+      res.json({ data: { id: deletedUser } });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = UserController;
