e006c405abd0627e32d1bccc77c49a29e784f648
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-09-26 18:01:16 -0300

    it is validated if it exists in db and new is updated

--- controllers/new.controller.js
+++ controllers/new.controller.js
@@ -52,6 +52,9 @@ class NewController {
     try {
       const findNew = await db.New.findByPk(id)
       if(!findNew) res.status(404).json({data: 'New Not Found'});
+      const updateNew = await findNew.update(changes);
+      delete updateNew.dataValues.deletedAt  //Elimina el envio de cuando fue eliminado al cliente.
+      res.status(200).json({msg: 'Novedad Actualizada con exito', data: updateNew})
     } catch (error) {
       next(error)
     }
