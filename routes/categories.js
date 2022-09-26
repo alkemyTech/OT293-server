58177351a0aa21ad3e9b24fef2e637b0e9f36c69
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-09-26 12:11:05 -0300

    POST Category

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const Authentication = require('../controllers/Categories');
 const router = express.Router();
 const CategoriesController = require('../controllers/categories.controller');
 const verifyAdmin = require('../middlewares/admin');
 
 router.delete('/:id/', verifyAdmin, CategoriesController.delete);
+router.post('/', Authentication.createCategory);
 
 module.exports = router;
