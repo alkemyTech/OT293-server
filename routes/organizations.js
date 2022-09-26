01643f53a5deea8e6c070ca077185710f58e0752
Santiago Mozo <smozo615@gmail.com>
2022-09-26 18:51:03 -0500

    Verify admin role to endpoint
      - Add middleware: "verifyAdmin" to POST organization/public/:id

--- routes/organizations.js
+++ routes/organizations.js
@@ -4,12 +4,14 @@ const { checkSchema } = require("express-validator");
 const OrganizationController = require("../controllers/organizationController");
 const { updateOrganizationSchema } = require("../schemas/organization.schema");
 const { dataValidator } = require("../middlewares/validator");
+const { verifyAdmin } = require("../middlewares/admin");
 
 const router = express.Router();
 
 router.get("/public", OrganizationController.findAll);
 router.post(
   "/public/:id",
+  verifyAdmin,
   checkSchema(updateOrganizationSchema),
   dataValidator,
   OrganizationController.update
