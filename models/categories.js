d29c930bce7d5581b0377301d71a7618e99c43f3
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-09-26 15:00:34 -0500

    root route to get categories is implemented

--- models/categories.js
+++ models/categories.js
@@ -14,7 +14,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Categories.init({
-    id: DataTypes.INTEGER,
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     image: DataTypes.STRING
