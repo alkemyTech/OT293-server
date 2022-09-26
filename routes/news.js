d9fc7182e057f22b33ea674d1db3b5764d005762
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-09-26 18:09:55 -0300

    route for news is added

--- routes/news.js
+++ routes/news.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const NewController = require('../controllers/new.controller')
 
-router.put('/news/:id', NewController.update);
+router.put('/:id', NewController.update);
 
 module.exports = router;
