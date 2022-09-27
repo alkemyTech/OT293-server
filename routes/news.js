e631504da6cd6a1cafd874767c393ea56a16430f
Santiago Mozo <smozo615@gmail.com>
2022-09-26 22:48:08 -0500

    Add data validator to endpoint: POST /news
      - Install "express-validator"
      - Create "schemas/new.schema.js"
      - Create "middlewares/validator.js"
      - Add middlewares: "checkSchema" and "dataValitor" to endpoint

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { checkSchema } = require("express-validator");
 
 const NewController = require("../controllers/new.controller");
+const { createNewSchema } = require("../schemas/new.schema");
+const { dataValidator } = require("../middlewares/validator");
 
 const router = express.Router();
 
-router.post("/", NewController.store);
+router.post("/", checkSchema(createNewSchema), dataValidator, NewController.store);
 
 module.exports = router;
