7ffe395654a3cfbdd5ff8ac92494b2841f1d6ac1
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-09-29 11:08:27 -0500

    the admin middleware import is resolved

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const { verifyAdmin } = require('../middlewares/admin');
+const verifyAdmin = require('../middlewares/admin');
 const NewController = require('../controllers/new.controller');
 const auth = require('../middlewares/auth');
 
