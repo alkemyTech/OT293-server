fb1af87531083a4c965ebe56cf9e914d042b71ca
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-09-30 20:26:04 -0300

    c

--- routes/categories.js
+++ routes/categories.js
@@ -7,5 +7,6 @@ const verifyAdmin = require('../middlewares/admin');
 router.get('/', auth, verifyAdmin, CategoriesController.findAll)
 router.post('/', verifyAdmin, CategoriesController.create);
 router.delete('/:id', verifyAdmin, CategoriesController.delete);
+router.get('/categories/:id', verifyAdmin, CategoriesController.findOne);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,4 @@
-'use strict';
-
-var express = require('express');
+const express = require('express');
 const { checkSchema } = require('express-validator');
 
 const ownership = require('../middlewares/ownership');
@@ -12,11 +10,14 @@ const UserController = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.get('/', verifyAdmin, UserController.getUsers);
+/* GET users listing. */
 
+router.delete('/:id', UserController.deleteUser);
+router.get('/', verifyAdmin, UserController.getUsers);
 router.post('/register', UserController.createUser);
 
-router.patch('/:id', 
+router.patch(
+  '/:id',
   ownership,
   checkSchema(updateUserSchema),
   UserController.partialUpdateUser,
