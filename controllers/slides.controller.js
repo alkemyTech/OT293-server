38cd7905c3775e53f209ba6be0a013cecbc84d5b
camila <camilafkapp@gmail.com>
2022-10-03 15:46:19 -0300

    Merge branch 'development' of https://github.com/alkemyTech/OT293-server into OT293-51/delete-new

---
+++ controllers/slides.controller.js
@@ -0,0 +1,55 @@
+const db = require('../models/index');
+
+class SlidesController {
+  static async findAll(req, res, next) {
+  }
+
+  static async findOne(req, res, next) {
+    try {
+
+      const { id } = req.params;
+
+      const slide = await db.Slide.findByPk(id);
+
+      if(!slide) {
+        return res.status(404).json({message: 'Slide not found'});
+      }
+
+      res.json({data: slide});
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async create(req, res, next) {
+
+  }
+
+  static async update(req, res, next) {
+
+  }
+
+  static async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+      const slide = await db.Slide.findOne({ where: { id } });
+      if (!slide) {
+        res.status(404).json({ error: 'Slide not found' });
+      }
+      const isDeleted = await db.Slide.destroy({ where: { id } });
+      if (!isDeleted) {
+        res.status(500).json({ error: 'Slide could not be deleted' });
+      }
+      res.json({
+        data: {
+          message: 'Slide has been deleted correctly',
+        },
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = SlidesController;
---
+++ routes/slides.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+//Middlewares
+const auth = require('../middlewares/auth');
+const verifyAdmin = require('../middlewares/admin');
+
+const SlideController = require('../controllers/slides.controller');
+
+const router = express.Router();
+
+router.get('/:id', 
+    auth,
+    verifyAdmin,
+    SlideController.findOne
+);
+
+router.delete('/:id', verifyAdmin, SlideController.delete);
+
+module.exports = router;
