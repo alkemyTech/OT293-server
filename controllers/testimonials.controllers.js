1b556c0cbd769a680ed536e32068039469abbe38
camila <camilafkapp@gmail.com>
2022-10-03 16:15:39 -0300

    adding post and put controller for testimonials

---
+++ controllers/testimonials.controllers.js
@@ -0,0 +1,58 @@
+const db = require("../models/index");
+
+class TestimonialsController {
+  static async create(req, res, next) {
+    try {
+      const { name, content } = req.body;
+      if (!name && !content) {
+        return res
+          .status(404)
+          .json({ message: "Name and content are required" });
+      }
+      const existentTestimonial = await db.Testimonials.findOne({
+        where: { name: name.toLowerCase() },
+      });
+
+      if (existentTestimonial) {
+        res.status(404).json({ message: "Testimonial already exists" });
+      } else {
+        const newTestimonial = await db.Testimonials.create({
+          name,
+          content,
+        })
+        res.json(newTestimonial);
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async findAll(req, res, next) {}
+
+  static async findOne(req, res, next) {}
+
+  static async update(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { name, content } = req.body;
+      const testimonial = await db.Testimonials.findByPk(id);
+
+      if (!testimonial) {
+        res.status(404).json({ message: "Testimonial not found" });
+      } else {
+        const updatedTestimonial = await testimonial.set({
+          name: name,
+          content: content,
+        });
+        await updatedTestimonial.save();
+        res.json(updatedTestimonial);
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async delete(req, res, next) {}
+}
+
+module.exports = TestimonialsController;
