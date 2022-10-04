0b7707882e74c61de6aae577b6fc902871c76bab
Santiago Mozo <smozo615@gmail.com>
2022-10-03 21:16:07 -0500

    Fix bugs

--- routes/slides.js
+++ routes/slides.js
@@ -2,23 +2,23 @@ const express = require("express");
 const { checkSchema } = require("express-validator");
 
 const SlidesController = require("../controllers/slides.controller");
-const verifyAdmin = require("../middlewares/admin");
 const { dataValidator } = require("../middlewares/validator");
 const { updateSlideSchema } = require("../schemas/slide.schema");
+const verifyAdmin = require("../middlewares/admin");
+const auth = require("../middlewares/auth");
 
-//Middlewares
-const auth = require('../middlewares/auth');
-const verifyAdmin = require('../middlewares/admin');
-const SlideController = require('../controllers/slides.controller');
 const router = express.Router();
 
-router.get('/', verifyAdmin, SlidesController.findAll);
-router.get('/:id', 
-    auth,
-    verifyAdmin,
-    SlideController.findOne
+router.get("/", verifyAdmin, SlidesController.findAll);
+router.get("/:id", auth, verifyAdmin, SlideController.findOne);
+router.put(
+  "/:id",
+  verifyAdmin,
+  checkSchema(updateSlideSchema),
+  dataValidator,
+  SlidesController.update
 );
 
-router.delete('/:id', verifyAdmin, SlideController.delete);
+router.delete("/:id", verifyAdmin, SlideController.delete);
 
 module.exports = router;
