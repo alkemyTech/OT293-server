c3335590095782ba2f9c7f5d110361699ab14a2b
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-10-05 11:21:57 -0300

    funcion sube archivos a aws

--- helpers/uploadImage.js
+++ helpers/uploadImage.js
@@ -1,20 +1,29 @@
-const { PutObjectCommand, ListObjectsCommand, GetObjectCommand } = require('@aws-sdk/client-s3')
-const fs = require('fs');
+const { S3Client ,PutObjectCommand, ListObjectsCommand, GetObjectCommand } = require('@aws-sdk/client-s3')
+const fs = require("fs");
 
 const config = require("../config/config").development;
 const client = require('../utils/s3');
 
-const uploaImage = async (imageFile) => {
-    const stream = fs.createReadStream(imageFile);
+// const client = new S3Client({
+//     region: config.awsBucketRegion,
+//     credentials: {
+//         accessKeyId: config.awsAccessKey,
+//         secretAccessKey: config.awsSecretAccessKey
+//     }
+// });
+
+const uploadImage = async (file) => {
+    console.log(file.tempFilePath)
+    const stream = fs.createReadStream(file.tempFilePath);
     const uploadParams = {
         Bucket: config.awsBucketName,
-        Key: `${file.name}`,
+        Key: file.name,
         Body: stream
     }
-    const command =new PutObjectCommand(uploadParams);
+    const command = new PutObjectCommand(uploadParams);
     const result = await client.send(command);
     console.log(result);
     
 }
 
-module.exports = uploaImage;
+module.exports = uploadImage;
