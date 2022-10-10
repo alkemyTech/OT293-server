81be5a74280f04e2c17a8bd21adc8501a5f426fa
Santiago Mozo <smozo615@gmail.com>
2022-10-10 00:00:43 -0500

    middleware: verifyAdmin
      - Modify middlewares/admin
      - Add auth to POST /organizations/public/:id

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,13 +1,12 @@
-'use strict';
-const {User} = require('../models/user');
-
 const verifyAdmin = async (req, res, next) => {
-    const user = await User.findById(req.roleId)
-    if (user === 1) {
-       next()
-    } else {
-        return res.status(403).json({message: 'You are not authorized to access this resource'})
-    }
-}
+  const { role } = req.user;
+  if (role === 1) {
+    next();
+  } else {
+    return res
+      .status(403)
+      .json({ message: 'You are not authorized to access this resource' });
+  }
+};
 
 module.exports = verifyAdmin;
--- routes/organizations.js
+++ routes/organizations.js
@@ -5,12 +5,14 @@ const OrganizationController = require("../controllers/organizationController");
 const { updateOrganizationSchema } = require("../schemas/organization.schema");
 const { dataValidator } = require("../middlewares/validator");
 const verifyAdmin = require("../middlewares/admin");
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.get("/public", OrganizationController.findAll);
 router.post(
   "/public/:id",
+  auth,
   verifyAdmin,
   checkSchema(updateOrganizationSchema),
   dataValidator,
