86d1032a20c20802c2e06c64ff325c5c4a88f708
Santiago Mozo <smozo615@gmail.com>
2022-10-09 22:16:09 -0500

    Add data validation to POST /auth/register
      - Create registerSchema -> schemas/auth.schema
      - Add middlewares: checkSchema and dataValidator to route

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { checkSchema } = require('express-validator');
 
 const AuthController = require('../controllers/auth.controller');
-const { loginSchema } = require('../schemas/auth.schema');
+const { loginSchema, registerSchema } = require('../schemas/auth.schema');
 const { dataValidator } = require('../middlewares/validator');
 // const UserController = require('../controllers/user.controller');
 
 const router = express.Router();
 
 // router.get('/me', UserController.getProfile);
-router.post('/register', AuthController.register);
+router.post('/register', checkSchema(registerSchema), dataValidator, AuthController.register);
 router.post('/login', checkSchema(loginSchema), dataValidator, AuthController.login);
 
 module.exports = router;
