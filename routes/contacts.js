72242cd6f7a53a44e2361647bbdeaab2ab5550cc
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-10-12 11:17:51 -0300

    Se modifica ruta de controller para crear un contacto

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,25 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ContactsController = require('../controllers/contacts.controller');
 const verifyAdmin = require('../middlewares/admin');
-const { checkSchema } = require('express-validator');
-
+const { checkSchema, body } = require('express-validator');
 //Middlewares
 const auth = require('../middlewares/auth');
 const { dataValidator } = require("../middlewares/validator");
 const { createContactSchema } = require('../schemas/contact.schema');
-const ContactController = require('../controllers/contact.controller');
 
-router.get(
-  "/", 
-  auth, 
-  verifyAdmin, 
-  ContactsController.findAll
-);
-
-router.post('/', 
-  auth, 
-  checkSchema(createContactSchema), 
-  dataValidator, ContactController.store
-);
+router.get("/", verifyAdmin, ContactsController.findAll);
+router.post('/', auth, checkSchema(createContactSchema), dataValidator, ContactsController.create);
 
 module.exports = router;
