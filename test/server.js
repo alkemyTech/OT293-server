a78975ac6d4826c2cae4d72c9315431b2fafe97e
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-10-16 21:14:03 -0500

    testing for auth endpoints

---
+++ test/server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+const app = require("../app");
+
+const server = http.createServer(app);
+const port = process.env.PORT || "3000";
+server.listen(port);
+
+module.exports = server;
