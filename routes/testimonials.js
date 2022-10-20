50bfe8b982c8867019c0881b26eaded546709703
lucasambrosini1 <87482540+lucasambrosini1@users.noreply.github.com>
2022-10-20 00:49:34 -0300

    Fix testimonials docs

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -7,39 +7,41 @@ const auth = require('../middlewares/auth');
 /**
  * @swagger
  * components:
- *   securitySchemes:
- *     bearerAuth:
- *     type: http
- *     scheme: bearer
- *     bearerFormat: JWT
- * schemas:
- *  createTestimonial:
- *   type: object
- *   properties:
- *    name:
- *      type: string
- *      description: Name of the testimonial
- *    content:
- *      type: string
- *      description: Content of the testimonial
- *    required:
- *     - name
- *     - content
- *    example:
- *     id: 1
- *     name: John Doe
- *     content: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- *   updateTestimonial:
- *    name:
- *      type: string
- *      description: New name of the testimonial
- *    content:
- *      type: string
- *      description: New content of the testimonial
- *    example:
- *     id: 1
- *     name: Jane Doe
- *     content: New description.
+ *   schemas:
+ *     createTestimonial:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Name of the testimonial
+ *           example: John Doe
+ *         content:
+ *           type: string
+ *           description: Content of the testimonial
+ *           example: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ *       required:
+ *       - name
+ *       - content
+ *     updateTestimonial:
+ *       type: object
+ *       properties:
+ *         id: 
+ *           type: integer
+ *           description: Id of the testimonial
+ *           example: 1
+ *         name:
+ *           type: string
+ *           description: New name of the testimonial
+ *           example: John Doe
+ *         content:
+ *           type: string
+ *           description: New content of the testimonial
+ *           example: New description.
+ *     securitySchemes:
+ *       bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
  */
 
 /**
@@ -52,31 +54,33 @@ const auth = require('../middlewares/auth');
 /** 
  * @swagger
  * /testimonials:
- * get:
- *  summary: Get all testimonials
- *    security:
- *    - bearerAuth: []
- *  tags: [Testimonials]
- * responses:
- *  200:
- *   description: The list of testimonials
- *    content:
- *     application/json:
- *     schema:
- *     type: array
+ *   get:
+ *     summary: Get all testimonials
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Testimonials]
+ *     responses:
+ *       200:
+ *         description: The list of testimonials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/createTestimonial'
  * 
- *  401:
- *   description: Unauthorized
- *    content:
- *      application/json:
- *      schema:
- *      type: object
- *      properties: 
- *        message:
- *        type: string
- *        example: Unauthorization. Please log in
- *  500:
- *   description: Internal server error
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties: 
+ *                 message:
+ *                   type: string
+ *                   example: Unauthorization. Please log in
+ *       500:
+ *         description: Internal server error
  */
 
 router.get('/', 
@@ -86,40 +90,39 @@ router.get('/',
 
 /**
  * @swagger
- * /testimonials
- * post:
- *  summary: Create a testimonial
- *  security:
- *   - bearerAuth: []
- *  tags: [Testimonials]
- *  requestBody:
- *    required: true
- *   content:
- *   application/json:
- *   schema:
- *   type: object
- *   $ref: '#/components/schemas/createTestimonial'
- * responses:
- *   200:
- *    description: The testimonial was successfully created
- *    content:
- *     application/json:
- *     schema:
- *     type: object
- *     $ref: '#/components/schemas/createTestimonial'
- *   404:
- *    description: Not found
- *    content:
- *      application/json:
- *      schema:
- *      type: object
- *      properties: 
- *      message:
- *      type: string
- *      example: Name and content are required
- * 
- *   500:
- *    description: Internal server error
+ * /testimonials:
+ *   post:
+ *     summary: Create a testimonial
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Testimonials]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/createTestimonial'
+ *     responses:
+ *       200:
+ *         description: The testimonial was successfully created
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                $ref: '#/components/schemas/createTestimonial'
+ *       404:
+ *         description: Not found
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties: 
+ *                  message:
+ *                    type: string
+ *                    example: Name and content are required
+ *       500:
+ *         description: Internal server error
  */
 router.post(
   '/', 
@@ -131,59 +134,59 @@ router.post(
 /** 
  * @swagger
  * /testimonials/{id}:
- * put:
- * summary: Update a testimonial
- * security:
- * - bearerAuth: []
- * tags: [Testimonials]
- *    parameters:
- *     - in: path
- *       name: id
- *       schema:
- *       type: integer
- *       required: true
- *       description: Id from the testimonial to update
- * responses:
- * 200:
- *    description: Successful request
- *     content:
- *      application/json:
- *      schema:
- *      type: object
- *      $ref: '#/components/schemas/updateTestimonial'
- * 401:
- *     description: Unauthorized
- *      content:
- *       application/json:
- *       schema:
- *       type: object
- *       properties: 
- *       message:
- *       type: string
- *       example: Unauthorization. Please log in
- * 403:
- *     description: Forbidden
- *      content:
- *       application/json:
- *       schema:
- *       type: object
- *       properties: 
- *       message:
- *       type: string
- *       example: You are not authorized to access this resource
+ *   put:
+ *     summary: Update a testimonial
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Testimonials]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Id from the testimonial to update
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                $ref: '#/components/schemas/updateTestimonial'
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties: 
+ *                  message:
+ *                    type: string
+ *                    example: Unauthorization. Please log in
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties: 
+ *                  message:
+ *                    type: string
+ *                    example: You are not authorized to access this resource
  * 
- * 404:
- *    description: Not found
- *     content:
- *      application/json:
- *      schema:
- *      type: object
- *      properties: 
- *      message:
- *      type: string
- *      example: Testimonial not found
- * 500:
- *    description: Internal server error
+ *       404:
+ *         description: Not found
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties: 
+ *                  message:
+ *                    type: string
+ *                    example: Testimonial not found
+ *       500:
+ *         description: Internal server error
 */
 router.put(
   '/:id', 
@@ -195,63 +198,62 @@ router.put(
 /**
  * @swagger
  * /testimonials/{id}:
- * delete:
- * summary: Delete a testimonial
- * security:
- * - bearerAuth: []
- * tags: [Testimonials]
+ *    delete:
+ *      summary: Delete a testimonial
+ *      security:
+ *        - bearerAuth: []
+ *      tags: [Testimonials]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Id from the testimonial to delete
  * 
- * parameters:
- * - in: path
- *  name: id
- *  schema:
- *  type: integer
- *  required: true
- *  description: Id from the testimonial to delete
- * 
- * responses:
- * 200:
- *    description: Successful request
- *     content:
- *      application/json:
- *      schema:
- *      type: object
- *      properties:
- *      message:
- *      type: string
- *      example: Testimonial eliminado correctamente
- * 401:
- *     description: Unauthorized
- *      content:
- *       application/json:
- *       schema:
- *       type: object
- *       properties: 
- *       message:
- *       type: string
- *       example: Unauthorization. Please log in
- * 403:
- *     description: Forbidden
- *      content:
- *       application/json:
- *       schema:
- *       type: object
- *       properties: 
- *       message:
- *       type: string
- *       example: You are not authorized to access this resource
- * 404:
- *    description: Not found
- *     content:
- *      application/json:
- *      schema:
- *      type: object
- *      properties: 
- *      message:
- *      type: string
- *      example: No existe un testimonial con ese ID
- * 500:
- *    description: Internal server error
+ *      responses:
+ *        200:
+ *          description: Successful request
+ *          content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   message:
+ *                     type: string
+ *                     example: Testimonial eliminado correctamente
+ *        401:
+ *          description: Unauthorized
+ *          content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties: 
+ *                   message:
+ *                     type: string
+ *                     example: Unauthorization. Please log in
+ *        403:
+ *          description: Forbidden
+ *          content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties: 
+ *                   message:
+ *                     type: string
+ *                     example: You are not authorized to access this resource
+ *        404:
+ *          description: Not found
+ *          content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties: 
+ *                   message:
+ *                     type: string
+ *                     example: No existe un testimonial con ese ID
+ *        500:
+ *          description: Internal server error
  */
 
 router.delete('/:id', 
