a1b3f7417d78f6cd1fc875fa2e9fedb740829c3a
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-10-20 12:23:56 -0500

    categories did not have softDeletes

--- models/categories.js
+++ models/categories.js
@@ -1,6 +1,4 @@
-const {
-  Model,
-} = require('sequelize');
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Categories extends Model {
@@ -13,13 +11,17 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Categories.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    image: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Categories',
-  });
+  Categories.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Categories",
+      paranoid: true,
+    }
+  );
   return Categories;
 };
