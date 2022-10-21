c613c481321d2f41593dff80ec4af31abdafc409
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-10-21 09:45:45 -0500

    bugs in slides is fixed

--- models/slide.model.js
+++ models/slide.model.js
@@ -1,5 +1,5 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Slide extends Model {
     /**
@@ -8,44 +8,48 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.Organization, {as: 'organization'});
+      this.belongsTo(models.Organization, { as: "organization" });
     }
-  };
-  Slide.init({
-    order: {
-      type: DataTypes.STRING
+  }
+  Slide.init(
+    {
+      order: {
+        type: DataTypes.STRING,
+      },
+      text: {
+        type: DataTypes.STRING,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        field: "image_url",
+      },
+      organizationId: {
+        type: DataTypes.INTEGER,
+        field: "organization_id",
+      },
+      deletedAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+        field: "deleted_at",
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: "updated_at",
+      },
     },
-    text: {
-      type: DataTypes.STRING
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      field: "image_url",
-    },
-    organizationId: {
-      type: DataTypes.INTEGER,
-      field: "organization_id",
-    },
-    deletedAt: {
-      allowNull: true,
-      type: DataTypes.DATE,
-      field: "deleted_at",
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-      field: "created_at",
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-      field: "updated_at",
-    },
-  }, {
-    sequelize,
-    modelName: 'Slide',
-    paranoid: true,
-    freezeTableName: true
-  });
+    {
+      sequelize,
+      modelName: "Slide",
+      paranoid: true,
+      freezeTableName: true,
+      tableName: "Slides",
+    }
+  );
   return Slide;
 };
